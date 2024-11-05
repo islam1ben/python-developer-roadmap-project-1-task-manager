03d415f834ba09ea385bc204395e134694b11787
islam1ben <129387053+islam1ben@users.noreply.github.com>
2024-11-05 11:30:56 +0100

    this is the final version of the base task class and its methods and its the back up file of this stage of the project

--- task manager project.py
+++ task manager project.py
@@ -1,18 +1,25 @@
+class InvalidStatusError(Exception):
+    def __init__(self, message="Status must be either 'done', 'not done', or 'in progress'"):
+        self.message = message
+        super().__init__(self.message)
+
 #creating task class 
+
 #the task can be modified with its number in the shown list to avoid creating more variables and make it easier
 class task:
-    task_id=0
-    check_box_status=None
-    status_positions = {'done','not done','in progress'}
-    def __init__ (self,status,task_title):
+    task_id_counter=0
+    
+
+    def __init__ (self,task_title):
         
         #static variable for the task id the changes for every task
-        task.task_id+=1
-        self.id=task.task_id
+        task.task_id_counter+=1
+        self.id=task.task_id_counter
         #make sure status is valid
-        if status not in task.status_positions :
-            raise ValueError('error , status must be either "done","not done" or "in progress"')
-        self.status = 'not done'
+        self.check_box_status=None
+        self.status='not done'
+        self.status_positions = {'done','not done','in progress'}
+        
         #task title can be null when initiated untill a text is affected to it 
         self.task_title=task_title
 
@@ -24,21 +31,32 @@ class task:
         elif self.status=='done':
             self.check_box_status='*'
         elif self.status=='in progress':
-            self.check_box_status='!'
+            self.check_box_status='-'
 
         print(f"[{self.check_box_status}]  {self.task_title}")
+
+    def update_task(self):
+        while True:
+            new_status = input('New status of the task: ')
+            try:
+                if new_status not in self.status_positions:
+                    raise InvalidStatusError()
+                self.status = new_status
+                break  # Exit loop if status is valid
+            except InvalidStatusError as e:
+                print(e)  # Inform the user of the error
+
+
+        new_task_title =input('new task title :')
+        self.task_title=new_task_title
 
 #task1=task('not done','give a gift to my brother')
 #task1.print_task()
 
-class task_list :
-    def __init__(self):
-        self.tasks=[]
 
-    def add_task(self,obj):
-        if isinstance(obj,task):
-            self.tasks.append(obj)
-        else:    
-            raise ValueError('you can only add a valid task formula to the tasks list')
-    def delete_item(self,obj_id):
-        self.tasks.id
+
+
+task1=task('do home work')
+task1.print_task()
+task1.update_task()
+task1.print_task()
