05e6da5c088bb370bbeb85736f1122a1d0d4ff0c
islam1ben <129387053+islam1ben@users.noreply.github.com>
2024-11-08 21:17:16 +0100

    done

--- task_manager_project.py
+++ task_manager_project.py
@@ -1,20 +1,22 @@
 import time 
 
+#the task list is a list of dictionaries each one contains task properies
 
 class task_list :
     current_list=[]
     def __init__(self):
         self.tasks=[]
-
+    #the id counter job is 
     task_id_counter=1
+
+    #adding task by giving it the creation time and default "todo" status
     def add_task(self):
-       
        temp_title = input ('enter your task :')
        temp_task= {'id':self.task_id_counter,'status':'todo','title':temp_title,'creation time':time.asctime(),'update time':None}
        self.tasks.append(temp_task)
        self.task_id_counter+=1
 
-
+    #deleting the task using its number in the list
     def delete_task (self):
         while True :
             task_num = int(input('type the number of the task you want to delete in this list:'))
@@ -23,7 +25,7 @@ class task_list :
             else :
                 print ('task not found, try again')
 
-
+        #basically removing the task by updating the list because i don't know any other way
         id_to_remove = self.current_list[task_num-1]['id']
         self.tasks=[item for item in self.tasks if item['id'] != id_to_remove]
         self.current_list=self.tasks
@@ -33,16 +35,18 @@ class task_list :
         self.i=1
         for item in self.current_list:
             print(f'{self.i}',end='.')
-            
+            #changing the checkbox of each task be=ased on its status
             if  self.current_list[self.i -1]['status']=='todo':
                 check_box_status=' '
             elif self.current_list[self.i -1]['status']=='done':
                 check_box_status='*'
             elif self.current_list[self.i -1]['status']=='in progress':
-                check_box_status='-'
+                check_box_status='~'
 
             print(f"[{check_box_status}]  {self.current_list[self.i -1]['title']}")
             self.i+=1
+
+    #changing the status of the task based on the used input and also giving it the update time 
     def update_task(self,task_num):
         operation =input('1-->done    2-->in progress    3-->todo\n')
         self.current_list[task_num-1]['update time']=time.asctime()
@@ -53,5 +57,5 @@ class task_list :
                 self.current_list[task_num-1]['status']='in progress'
             case '3':
                 self.current_list[task_num-1]['status']='todo'
-    #        case :
-    #            print('error!, invalid input')
+            case _:
+                print('error!, invalid input')
--- user interface.py
+++ user interface.py
@@ -2,43 +2,64 @@ from task_manager_project import *
 import os
 import json
 
+#function to clear the output every cycle
 def clear_output():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+#creating the task list
 user_list = task_list()
 
+#setting current list to empty list so that is satisfies the condition in line 31
 user_list.current_list = []
 
-def load_data(data="data.json"):
+#function of loading data from the file if existing if not returns an empty list
+def load_data(user_list,data_file="data.json"):
     try:
-        with open(data, "r") as file:
-            return json.load(file)
+        with open(data_file, "r") as file:
+            data = json.load(file)
+            user_list.tasks = data.get("tasks")
+            user_list.task_id_count = data.get("id_count")
     except FileNotFoundError:
-        return []
+        user_list.tasks
     
 
-def save_data(data, filename="data.json"):
+
+#function of saving data into the same file so that if its not existing it will create it
+def save_data(user_list, filename="data.json"):
+    data ={
+        "tasks":user_list.tasks,
+        "id_count": user_list.task_id_counter
+    }
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
-user_list.tasks=load_data()
+
+#loading the data from the file
+load_data(user_list)
 
 while True :
+    #if list is empty sets it to the main list 
     if user_list.current_list==[None]:
         user_list.current_list=user_list.tasks
     else :pass
 
+    #decoreting the output ,well ... kinda
     print('\t\t\tToDo List\n\n')
     user_list.print_list()
     print('\n\n')
 
     process = input('1-->add task\n2-->update task\n3-->remove task\n4-->show full list\n5-->show "todo" list\n6-->show "done" list\n7-->show "in progress" list\n8-->quite task manager\n')
 
+
+    #creating an infinit loop only close if the user chose to 
     match process:
         case '1':
             user_list.add_task()
+
+
         case '2':
+            #get the number of the task to update
             while True :
                 task_num = int(input('type the number of the task you want to update in this list:'))
                 if task_num<= user_list.i and task_num >0:
@@ -47,6 +68,7 @@ while True :
                     print ('task not found, try again')
 
             user_list.update_task(task_num)
+
 
         case '3':
             user_list.delete_task()
@@ -62,7 +84,6 @@ while True :
             break
     clear_output()
 
-save_data(user_list.tasks)
+save_data(user_list)
 
 print('---your data has been saved---')
-
